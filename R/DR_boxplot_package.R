ace87e17954e9a79982659fee8e1c4534adb1c8a
Alonso Arriagada <alonso.arriagada@usach.cl>
2024-12-17 14:20:01 +0000

    version 1.1.0

--- R/DR_boxplot_package.R
+++ R/DR_boxplot_package.R
@@ -1,9 +1,9 @@
-#' Plot the result from the application of the Deductive Rational Method
+#' BoxPlot from the result from the application of the Deductive Rational Method
 #' @export
 #' @param dr data frame from DR function
 #' @import dplyr reshape2 ggplot2
 
-DR_plot <- function(dr){
+DR_boxplot <- function(dr){
   data_long_month <- dr %>% melt(id.vars=c("year"))
   colnames(data_long_month)[2:3] <- c("Month", "Value")
   data_long_month$year <- as.numeric(data_long_month$year)
@@ -12,11 +12,10 @@ DR_plot <- function(dr){
   colnames(aux) <- c("Month","Mean")
   aux$Mean <- as.numeric(aux$Mean)
 
-  ggplot(data_long_month, aes(x=as.factor(data_long_month$Month), y=data_long_month$Value, group=data_long_month$year, color=data_long_month$year)) +
-    labs(color='year') +
-    geom_line(size = 1, alpha = 0.75) +
+  ggplot(data_long_month, aes(x=as.factor(data_long_month$Month), y=data_long_month$Value)) +
+    geom_boxplot(outlier.size = 1, outlier.alpha = 0.2, fill = "green") +
     scale_x_discrete(labels=month.abb[seq(1,12)], guide = guide_axis(angle = 90)) +
-    geom_line(data = aux,aes(x=factor(aux$Month, levels = month.abb),y=aux$Mean,fill="mean"),size = 1.75, color = "red", linetype = "dashed", group =1,show.legend = TRUE) +
+    geom_line(data = aux,aes(x=factor(aux$Month, levels = month.abb),y=aux$Mean, fill="mean"),size = 1.75, color = "red", linetype = "dashed", group =1,show.legend = TRUE) +
     scale_fill_manual("line", values=c(1),guide=guide_legend(override.aes = list(colour=c("red")))) +
     ggtitle("Monthly values filled with Deductive Rational Method") +
     xlab("Month") + ylab("Value") +
